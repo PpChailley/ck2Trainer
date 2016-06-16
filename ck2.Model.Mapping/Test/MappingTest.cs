e1fc8655bd0005ffe9a6813bd466933049b11f9e
PP. Chailley <ppchailley@gmx.com>
2016-06-16 11:37:32 +0300

    Stash unk6nown changes

--- ck2.Model.Mapping/Test/MappingTest.cs
+++ ck2.Model.Mapping/Test/MappingTest.cs
@@ -1,6 +1,45 @@
-﻿namespace ck2.Mapping.Save.Test
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using NUnit.Framework;
+
+namespace ck2.Mapping.Save.Test
 {
-    class MappingTest
+
+    [TestFixture]
+    public class MappingTest
     {
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Nothing yet
+        }
+
+        [Test]
+        public void SmokeTest()
+        {
+            Save s = new Save();
+        }
+
+
+        [Test]
+        public void LoadFile()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        [Test]
+        public void InterrogateModel()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void NAME()
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }

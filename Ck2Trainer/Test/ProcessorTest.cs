edd4410f97ea4bc6b29023d773e10c10ad7f5e58
PPC <ppchailley@gmx.com>
2016-09-09 03:43:59 +0200

    TODO Add tests for next steps

---
+++ Ck2Trainer/Test/ProcessorTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Ck2.Trainer.Test
+{
+    [TestFixture]
+    class ProcessorTest
+    {
+
+        [SetUp]
+        public void Setup()
+        { }
+
+        [Test]
+        public void SmokeTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void IntegrateProcessor()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
